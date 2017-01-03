19b9aad5b6f39ca5181e167c308331aa2ab9946c
mattlaguardia <matthewlaguardia@gmail.com>
2017-01-02 18:11:28 -0800

    finishes small app of drum kit

--- day-01-drum-kit/main.js
+++ day-01-drum-kit/main.js
@@ -1,1 +1,21 @@
 console.log("Connected")
+
+window.addEventListener('keydown', function (e) {
+  // Attribute Selectory in CSS = audio[data-key="65"]
+  // ES6 Template strings = `${}` -- needs back ticks
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio){ return }
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
+})
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform'){ return }
+  this.classList.remove('playing')
+  console.log(this)
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
